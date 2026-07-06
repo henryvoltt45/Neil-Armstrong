091c4cfd008a8565efd42d5b332de1c8c2440ef4
NamaDia <spartahenry9373@gmail.com>
2026-07-07 02:28:01 +0700

    07.07.26

--- problems/1950D.cpp
+++ problems/1950D.cpp
@@ -3,48 +3,52 @@
 #include <unordered_map>
 using namespace std;
 
-vector<int> binaryDecimal;
+vector<int> binaryDecimals;
 unordered_map<int, bool> dp;
 
-bool isbinarydecimal(int x){
-    while(x > 0){
+bool isBinaryDecimal(int x) {
+    while (x > 0) {
         int d = x % 10;
-        if(d != 0 && d != 1)
+        if (d != 0 && d != 1)
             return false;
         x /= 10;
     }
     return true;
 }
 
-bool dfs(int x){
+bool dfs(int x) {
     if (x == 1)
         return true;
-    if(dp.count(x))
+
+    if (dp.count(x))
         return dp[x];
 
-    for(int b : binaryDecimal){
-        if(b == 1) continue;
-        if(x % b == 0){
-            if(dfs(x / b))
+    for (int b : binaryDecimals) {
+        if (b == 1) continue; // hindari loop tak hingga
+        if (x % b == 0) {
+            if (dfs(x / b))
                 return dp[x] = true;
         }
     }
+
     return dp[x] = false;
 }
 
-int main(){
-    for(int i = 0; i <= 100000; i++){
-        if(isbinarydecimal(i))
-            binaryDecimal.push_back(i);
+int main() {
+    // Generate semua binary decimal <= 100000
+    for (int i = 1; i <= 100000; i++) {
+        if (isBinaryDecimal(i))
+            binaryDecimals.push_back(i);
     }
+
     int t;
     cin >> t;
 
-    while(t--){
+    while (t--) {
         int n;
         cin >> n;
-        cout << (dfs(n) ? "YES" : "NO") << endl;
+        cout << (dfs(n) ? "YES" : "NO") << '\n';
     }
+
     return 0;
 }
-
